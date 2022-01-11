b1c9b5e207e23858599ab90b960beeac8c07651b
lguenth <mail@lukeguenther.de>
2022-01-11 15:58:09 +0100

    test deploy v0.1

--- tweetsort/streamlit.py
+++ tweetsort/streamlit.py
@@ -4,6 +4,7 @@ import pandas as pd
 # TODO Tags implementieren, siehe: https://github.com/gagan3012/streamlit-tags
 # TODO Pfade implementieren
 # TODO Heroku deployen
+# TODO Index in CSV schreiben
 
 
 def main():
@@ -26,7 +27,12 @@ def main():
     if "df" not in st.session_state:
         st.session_state.df = pd.read_csv(corpus, sep=";", dtype=object)
 
-    st.title("Tweetsort – Korpus filtern leichtgemacht")
+    st.title("Tweetsort: Korpus filtern leichtgemacht")
+
+    keep = st.session_state.keep
+    keep_df = st.session_state.keep_df
+    discard = st.session_state.discard
+    discard_df = st.session_state.discard_df
 
     if st.session_state.index <= st.session_state.df.shape[0]:
         st.write(f"Tweet mit Index {st.session_state.index}:")
@@ -36,58 +42,58 @@ def main():
         )
 
         if st.button("Behalten"):
-            st.session_state.keep.append(
+            keep.append(
                 st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
             )
-            st.session_state.keep_df.loc[
-                len(st.session_state.keep_df.index)
-            ] = st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
+            keep_df.loc[len(keep_df.index)] = st.session_state.df["text_clean"].iloc[
+                st.session_state.index - 1
+            ]
 
         if st.button("Aussortieren"):
-            st.session_state.discard.append(
+            discard.append(
                 st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
             )
-            st.session_state.discard_df.loc[
-                len(st.session_state.discard_df.index)
-            ] = st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
+            discard_df.loc[len(discard_df.index)] = st.session_state.df[
+                "text_clean"
+            ].iloc[st.session_state.index - 1]
 
         if st.button("Speichern"):
-            st.session_state.keep_df = st.session_state.keep_df.drop_duplicates()
-            st.session_state.discard_df = st.session_state.discard_df.drop_duplicates()
-            st.session_state.keep_df.to_csv(
+            keep_df = keep_df.drop_duplicates()
+            discard_df = discard_df.drop_duplicates()
+            keep_df.to_csv(
                 "/home/lukel/projekte/twittersmh/data/tweepy_corpus_saved_tweets.csv"
             )
-            st.session_state.discard_df.to_csv(
+            discard_df.to_csv(
                 "/home/lukel/projekte/twittersmh/data/tweepy_corpus_discarded_tweets.csv"
             )
             st.write("Tweets gespeichert!")
             st.session_state.index -= 1
 
-        if st.button("Cache leeren"):
-            # st.legacy_caching.clear_cache()
+        if st.button("Cache leeren und Seite neu laden"):
+            st.legacy_caching.clear_cache()
             st.session_state.index = 0
-            st.session_state.keep = []
-            st.session_state.discard = []
-            st.session_state.keep_df = st.session_state.keep_df.drop_duplicates()
-            st.session_state.discard_df = st.session_state.discard_df.drop_duplicates()
+            keep = []
+            discard = []
+            keep_df = keep_df.drop_duplicates()
+            discard_df = discard_df.drop_duplicates()
             st.experimental_rerun()
 
     elif st.session_state.index == st.session_state.df.shape[0]("Speichern"):
         st.write("Alle Tweets sortiert und gespeichert!")
-        st.session_state.keep_df.to_csv(
+        keep_df.to_csv(
             "/home/lukel/projekte/twittersmh/data/tweepy_corpus_saved_tweets.csv"
         )
-        st.session_state.discard_df.to_csv(
+        discard_df.to_csv(
             "/home/lukel/projekte/twittersmh/data/tweepy_corpus_discarded_tweets.csv"
         )
 
     st.markdown("## Aussortiert")
     st.expander("aussortiert")
-    st.write(st.session_state.discard)
+    st.write(list(set(discard)))
 
     st.markdown("## Behalten")
     st.expander("behalten")
-    st.write(st.session_state.keep)
+    st.write(set(keep))
     st.session_state.index += 1
 
 
