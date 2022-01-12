5dfe2f99d9961b6522163ae28db5fa0dd550eac4
lguenth <mail@lukeguenther.de>
2022-01-12 17:16:52 +0100

    updated tweetsort

--- tweetsort/streamlit.py
+++ tweetsort/streamlit.py
@@ -27,12 +27,7 @@ def main():
     if "df" not in st.session_state:
         st.session_state.df = pd.read_csv(corpus, sep=";", dtype=object)
 
-    st.title("Tweetsort: Korpus filtern leichtgemacht")
-
-    keep = st.session_state.keep
-    keep_df = st.session_state.keep_df
-    discard = st.session_state.discard
-    discard_df = st.session_state.discard_df
+    st.title("Tweetsort – Korpus filtern leichtgemacht")
 
     if st.session_state.index <= st.session_state.df.shape[0]:
         st.write(f"Tweet mit Index {st.session_state.index}:")
@@ -42,28 +37,28 @@ def main():
         )
 
         if st.button("Behalten"):
-            keep.append(
+            st.session_state.keep.append(
                 st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
             )
-            keep_df.loc[len(keep_df.index)] = st.session_state.df["text_clean"].iloc[
-                st.session_state.index - 1
-            ]
+            st.session_state.keep_df.loc[
+                len(st.session_state.keep_df.index)
+            ] = st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
 
         if st.button("Aussortieren"):
-            discard.append(
+            st.session_state.discard.append(
                 st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
             )
-            discard_df.loc[len(discard_df.index)] = st.session_state.df[
-                "text_clean"
-            ].iloc[st.session_state.index - 1]
+            st.session_state.discard_df.loc[
+                len(st.session_state.discard_df.index)
+            ] = st.session_state.df["text_clean"].iloc[st.session_state.index - 1]
 
         if st.button("Speichern"):
-            keep_df = keep_df.drop_duplicates()
-            discard_df = discard_df.drop_duplicates()
-            keep_df.to_csv(
+            st.session_state.keep_df = st.session_state.keep_df.drop_duplicates()
+            st.session_state.discard_df = st.session_state.discard_df.drop_duplicates()
+            st.session_state.keep_df.to_csv(
                 "/home/lukel/projekte/twittersmh/data/tweepy_corpus_saved_tweets.csv"
             )
-            discard_df.to_csv(
+            st.session_state.discard_df.to_csv(
                 "/home/lukel/projekte/twittersmh/data/tweepy_corpus_discarded_tweets.csv"
             )
             st.write("Tweets gespeichert!")
@@ -72,28 +67,28 @@ def main():
         if st.button("Cache leeren und Seite neu laden"):
             st.legacy_caching.clear_cache()
             st.session_state.index = 0
-            keep = []
-            discard = []
-            keep_df = keep_df.drop_duplicates()
-            discard_df = discard_df.drop_duplicates()
+            st.session_state.keep = []
+            st.session_state.discard = []
+            st.session_state.keep_df = st.session_state.keep_df.drop_duplicates()
+            st.session_state.discard_df = st.session_state.discard_df.drop_duplicates()
             st.experimental_rerun()
 
     elif st.session_state.index == st.session_state.df.shape[0]("Speichern"):
         st.write("Alle Tweets sortiert und gespeichert!")
-        keep_df.to_csv(
+        st.session_state.keep_df.to_csv(
             "/home/lukel/projekte/twittersmh/data/tweepy_corpus_saved_tweets.csv"
         )
-        discard_df.to_csv(
+        st.session_state.discard_df.to_csv(
             "/home/lukel/projekte/twittersmh/data/tweepy_corpus_discarded_tweets.csv"
         )
 
     st.markdown("## Aussortiert")
     st.expander("aussortiert")
-    st.write(list(set(discard)))
+    st.write(st.session_state.discard)
 
     st.markdown("## Behalten")
     st.expander("behalten")
-    st.write(set(keep))
+    st.write(st.session_state.keep)
     st.session_state.index += 1
 
 
